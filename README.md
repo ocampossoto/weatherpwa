8abbaf1cd9c2397ff38963d92febf72326d2fea9
Oscar Campos-Soto <ocampossoto1@gmail.com>
2023-08-16 21:36:46 -0500

    feat: :recycle: Moving to vite upgrading packages to latest.

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-![Deployed](https://github.com/ocampossoto/weatherpwa/workflows/Deploy%20to%20Firebase%20Hosting%20on%20merge/badge.svg)
+[![Deploy to Firebase Hosting on PR](https://github.com/ocampossoto/weatherpwa/actions/workflows/firebase-hosting-pull-request.yml/badge.svg)](https://github.com/ocampossoto/weatherpwa/actions/workflows/firebase-hosting-pull-request.yml)
 
 # weatherpwa
+
 A simple website that tells you the current weather when you provide a city or your location. It uses the [Open Weather Map API](https://openweathermap.org/ "Open Weather Map API") to get the current weater.
 Live website: [Here](https://ocampossoto-weather.web.app/ "Weather App")
 
 ## Features
-- Progressive Web App (PWA) you can install it as an app on any device that supports it. 
-- Uses the browser location API to get the users location. 
+
+- Progressive Web App (PWA) you can install it as an app on any device that supports it.
+- Uses the browser location API to get the users location.
 
 ## Tech
-- React (TypeScript) 
+
+- React (TypeScript)
 - Firebase
   - Hosting the static files
   - Storing and providing API keys
--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,52 @@
-import React, {Suspense} from 'react';
-// import './App.css';
-import firebase from 'firebase/app'
-import "firebase/database"
-import DarkTheme from "./darktheme";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { Grid, Button } from '@material-ui/core';
-import CssBaseline from "@material-ui/core/CssBaseline";
-import CircularProgress from '@material-ui/core/CircularProgress';
-const WeatherMain = React.lazy(() => import('./weatherMain'));
-const firebaseConfig = {
+import {
+  CircularProgress,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+} from "@mui/material";
+import { getAnalytics } from "firebase/analytics";
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import { Suspense } from "react";
+import WeatherMain from "./weatherMain";
+
+const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCFzMeckRfCYw29HFUfHRslnXPrN-Wuz-M",
   authDomain: "weatherappproj2021.firebaseapp.com",
   projectId: "weatherappproj2021",
   storageBucket: "weatherappproj2021.appspot.com",
   messagingSenderId: "266354659024",
   appId: "1:266354659024:web:1ddbe6ca228646f75f18d5",
-  measurementId: "G-HY17J2PGFX"
+  measurementId: "G-HY17J2PGFX",
 };
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}else {
-  firebase.app(); // if already initialized, use that one
-}
 
 function App() {
-  const [key, setKey] = React.useState();
-  const [darkState, setDarkState] = React.useState(false);
-  let darkTheme:any = DarkTheme(darkState);
-  let darkmode:boolean = false;
-  React.useEffect(()=>{
-    setDarkState(darkmode)
-  },[darkmode])
-  const handleThemeChange = () => {
-    setDarkState(!darkState);
-  };
-  React.useEffect(()=>{
-    firebase.database().ref("weatherAPIKEY").once('value').then((snapshot) => {
-      setKey(snapshot.val());
+  const app = initializeApp(firebaseConfig);
+  getAnalytics(app);
 
-    });
-    
-  },[])
-
-  return (<ThemeProvider theme={darkTheme}>
-    <CssBaseline/>
-    <Grid container alignItems="center" justify="center" >
-      <Grid item lg={12} md={12} sm={12} xs={12} style={{justifyContent: "center", textAlign: "center", paddingTop: "5%"}}>
-        
-        <Button variant="contained" onClick={() => {handleThemeChange()}}>{darkState? "Light Theme": "Dark Theme"}</Button>
-      </Grid>
-      <Suspense fallback={
-          <CircularProgress 
-            style={{marginTop: "25%",marginBottom: "30%", marginLeft: "50%", justifyContent: "center"}} 
-            color="inherit"/>}>
-            <WeatherMain APIkey={key} />
-          
+  return (
+    <ThemeProvider theme>
+      <CssBaseline />
+      <Grid
+        container
+        width="100vw"
+        alignContent="center"
+        justifyContent="center"
+      >
+        <Suspense
+          fallback={
+            <CircularProgress
+              style={{
+                marginTop: "25%",
+                marginBottom: "30%",
+                marginLeft: "50%",
+                justifyContent: "center",
+              }}
+              color="inherit"
+            />
+          }
+        >
+          <WeatherMain />
         </Suspense>
-    
-        
       </Grid>
     </ThemeProvider>
   );
